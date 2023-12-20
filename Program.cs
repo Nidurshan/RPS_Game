f9fcf1b3365cb1ac71a819f9e24920d451cf4128
Nidurshan <nidurshanpro@gmail.com>
2023-12-20 19:52:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿
+using RPS_Game;
+
+var exit = false;
+
+var weaponCodes = new List<string> { "R", "P", "S" };
+
+var weaponMaps = new Dictionary<string, Weapon>
+{
+    { "R", new Weapon("rock") },
+    { "P", new Weapon("paper") },
+    { "S", new Weapon("scissor") }
+};
+
+while (!exit)
+{
+    Console.WriteLine();
+    Console.WriteLine("..................................................................");
+    Console.WriteLine("..................................................................");
+    Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>> ROCK PAPER SCISSOR <<<<<<<<<<<<<<<<<<<<<<<");
+    Console.WriteLine("..................................................................");
+    Console.WriteLine("..................................................................");
+    Console.WriteLine();
+
+    Console.Write("Enter your name: ");
+    var player = Console.ReadLine();
+    var opponent = "Computer";
+
+    var playerScore = 0;
+    var opponentScore = 0;
+    
+    var scoreBoard = $"{player}   {playerScore} vs {opponentScore}   {opponent}";
+
+    Console.WriteLine("");
+
+    var times = 3;
+
+    for (var i=0; i<times; i++)
+    {
+        Console.Write($"{player} => ");
+        var playerWeaponCode = Console.ReadLine().ToUpper();
+        var playerWeapon = weaponMaps[playerWeaponCode];
+
+        var random = new Random().Next(1, 4);
+        var randomWeaponCode = weaponCodes[random - 1];
+        var opponentWeapon = weaponMaps[randomWeaponCode];
+
+        var battleWinner = GuessBattleWinner(playerWeapon, opponentWeapon);
+
+
+        if (battleWinner == "opponent")
+        {
+            opponentScore++;
+            scoreBoard = $"{player}   {playerScore} vs {opponentScore}   {opponent}";
+            Console.WriteLine($"{opponent}'s {opponentWeapon.Name} beats {player}'s {playerWeapon.Name} \n---------------------------------------- {scoreBoard}");
+        }
+        else if (battleWinner == "player")
+        {
+            playerScore++;
+            scoreBoard = $"{player}   {playerScore} vs {opponentScore}   {opponent}";
+            Console.WriteLine($"{player}'s {playerWeapon.Name} beats {opponent}'s {opponentWeapon.Name} \n---------------------------------------- {scoreBoard}");
+        }
+        else 
+        {
+            Console.WriteLine($"It's a draw \n---------------------------------------- {scoreBoard}");
+        }
+
+        Console.WriteLine();
+    }
+
+    string warWinner = GuessWarWinner(playerScore, opponentScore);
+    var result = warWinner == null 
+        ? "It's a draw :|" 
+        : warWinner == "player" 
+            ? "You won :)" : "You lose :(";
+
+    Console.WriteLine();
+    Console.WriteLine($"========================================================================>>> {result}");
+    Console.WriteLine($"========================================================================>>> {scoreBoard}");
+
+    Console.WriteLine();
+    Console.Write("Play again? [Y/N]: ");
+    exit = Console.ReadLine().ToUpper() == "Y" ? false : true;
+
+}
+
+string GuessBattleWinner(Weapon playerWeapon, Weapon opponentWeapon)
+{
+    if(playerWeapon.Superior == opponentWeapon.Name)
+    {
+        return "opponent";
+    }
+    else if (playerWeapon.Inferior == opponentWeapon.Name)
+    {
+        return "player";
+    }
+    else
+    {
+        return null;
+    }
+}
+
+string GuessWarWinner(int playerScore, int opponentScore)
+{
+    if (playerScore < opponentScore) return "opponent";
+    else if (playerScore > opponentScore) return "player";
+    else return null;
+}
+
---
+++ Weapon.cs
@@ -0,0 +1,36 @@
+﻿namespace RPS_Game
+{
+    public class Weapon
+    {
+
+        private readonly Dictionary<string, string[]> rules = new Dictionary<string, string[]>
+        {
+            { "rock", new string[2] { "paper", "scissor" } },
+            { "paper", new string[2] { "scissor", "rock" } },
+            { "scissor", new string[2] { "rock", "paper" } }
+        };
+
+        private readonly Dictionary<string, string> codes = new Dictionary<string, string>
+        {
+            { "rock", "R" },
+            { "paper", "P" },
+            { "scissor", "S" }
+        };
+
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Superior { get; set; } 
+        public string Inferior { get; set; }
+
+        public Weapon(string name)
+        {
+            Name = name;
+            Code = codes[name];
+            Superior = rules[name][0];
+            Inferior = rules[name][1];
+
+            //Console.WriteLine($"{Name}({Code}) is \nsuperior to {Superior}({codes[Superior]}) and \ninferior to {Inferior}({codes[Inferior]})");
+        }
+    }
+}
+
